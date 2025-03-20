6613ed545c40d9f879271a0a9da007ae206b4a85
Enzo Ferreira Santos <enzogelp483@gmail.com>
2025-03-20 00:49:48 -0300

    inciso 3 y correciones

--- questions.py
+++ questions.py
@@ -23,22 +23,14 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, en el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
-6.
-
-7.
-
-1.
-2.
-3.
-
 # El usuario deberá contestar 3 preguntas
 puntaje = 0
-for _ in range(3):
-# Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
-# Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+# Se selecciona una pregunta aleatoria, ayuda de la práctica
+questions_to_ask = random.choices(list(zip(questions,answers, correct_answers_index)), k=3)
+# Se muestra la pregunta y las respuestas posibles, se deben contestar 3 preguntas
+for pregunta,opciones,respuesta in questions_to_ask:
+    print(pregunta)
+    for i, answer in enumerate(opciones):
         print(f"{i + 1}. {answer}")
 # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
@@ -46,7 +38,7 @@ for _ in range(3):
         if user_answer in ["1","2","3","4"]:
             user_answer = int(user_answer)-1
 # Se verifica si la respuesta es correcta
-            if user_answer == correct_answers_index[question_index]:
+            if user_answer == respuesta:
                 print("¡Correcto!")
                 puntaje+=1
                 break
@@ -59,7 +51,7 @@ for _ in range(3):
             sys.exit(1)
     else: 
         print("Incorrecto. La respuesta correcta es: ")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(opciones[respuesta])
 
 # Se imprime un blanco al final de la pregunta
 print()
