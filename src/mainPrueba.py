630f18753b9c493e54054557f4fab2ebf1d66948
Enzo Ferreira Santos <enzogelp483@gmail.com>
2025-04-03 03:12:23 -0300

    Separando codigo ejemplo

---
+++ src/mainPrueba.py
@@ -0,0 +1,14 @@
+#IMPORTAR FUNCIONES SI MAIN FUERA DE LA CARPETA SRC
+
+
+
+dataset = [
+        { "name": "Carlos",
+         "age": "25",
+         "email":"carlos@example.com",
+         "country":"argentina",
+         },
+        ]
+
+
+print(clean_data(dataset))
